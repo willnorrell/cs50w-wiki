2b95e96bd1814c9a1d591b0102b3c5b3d71e1d29
Will Norrell <willnorrell@Wills-MBP.lan>
2023-02-21 16:02:48 +0000

    Began on Markdown & Added Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
+
+def converter(name):
+    file = util.get_entry(name)
+    markdowner = Markdown()
+    html_file = markdowner.convert(file)
+    return html_file
+
 
 
 def index(request):
@@ -8,3 +15,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    entries = util.list_entries()
+    if title in entries:
+        return  
